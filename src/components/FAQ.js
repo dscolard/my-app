e86d6a8699c00973f2a98ad857d6c9ba4f059db9
David Scolard <dscolard@tcd.ie>
2020-02-02 22:35:18 +0000

    readme

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -12,7 +12,12 @@ class FAQ extends React.Component {
         <h1>Frequently Asked Questions</h1>
         <div style={{padding:20}}>
             <p>
-            What is data quality?<br/>...
+            What is Motor Neruone Disease?<br/>
+            Motor neurones are specialised nerve cells that control important voluntary muscle activity 
+            such as walking, talking and breathing. MND refers to any disease which inhibits these nerve cells and prevents them from operating 
+            normally. The loss of motor neurone function leads to increasing loss of mobility in the limbs and difficulty with speech, 
+            swallowing and breathing. Ultimately the prognosis is death, 70% of people with ALS (The most common form of MND) die within three 
+            years after the first symptoms present themselves.
             </p>
             <p>
                 How do you measure data quality?<br/>...
